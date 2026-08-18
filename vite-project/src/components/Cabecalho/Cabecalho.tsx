756c4cc94da6b003332eddca649badf44a964fef
Nycolas10072006 <nycolasmescobar@gmail.com>
2026-08-18 20:59:22 -0300

    feat:criação do boiler plate

---
+++ vite-project/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,16 @@
+export default function Cabecalho(){
+    return(
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+}
