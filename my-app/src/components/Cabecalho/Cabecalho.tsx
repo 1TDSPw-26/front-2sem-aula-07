d9b3eb2dede4828a75c72c5767ecf63e733a13c2
NickRM22 <nick.rodrigues2005@gmail.com>
2026-08-18 20:57:32 -0300

    feat: Adicionando componentes

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,15 @@
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Item-1</a></li>
+                    <li><a href="">Item-2</a></li>
+                    <li><a href="">Item-3</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,9 @@
+export default function Conteudo(){
+
+    return(
+        <main>
+            <h2>Este é o conteúdo</h2>
+            <p>Conteudão</p>
+        </main>
+    )
+}
