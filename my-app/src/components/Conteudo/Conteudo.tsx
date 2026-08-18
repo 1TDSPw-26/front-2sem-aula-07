2ba01c065b1d9a8e56e3e66d600ffd3d0aff2628
thssd <thiagoshimizusodre@gmail.com>
2026-08-18 20:59:32 -0300

    feat: Integração do boiler plate.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,14 @@
+export default function Conteudo() {
+    return (
+        <main>
+            <div>
+                <h2>Titulo 1</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam dolorem sequi exercitationem, tenetur perferendis iste culpa doloribus accusantium ullam illo recusandae laboriosam, quas vero voluptas necessitatibus eos blanditiis. Ratione consequuntur quaerat, placeat adipisci deserunt ad quia explicabo quam culpa cupiditate quas numquam quod quidem, suscipit aut sint dignissimos ipsam. Enim!</p>
+                <h2>Titulo 2</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam dolorem sequi exercitationem, tenetur perferendis iste culpa doloribus accusantium ullam illo recusandae laboriosam, quas vero voluptas necessitatibus eos blanditiis. Ratione consequuntur quaerat, placeat adipisci deserunt ad quia explicabo quam culpa cupiditate quas numquam quod quidem, suscipit aut sint dignissimos ipsam. Enim!</p>
+                <h2>Titulo 3</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam dolorem sequi exercitationem, tenetur perferendis iste culpa doloribus accusantium ullam illo recusandae laboriosam, quas vero voluptas necessitatibus eos blanditiis. Ratione consequuntur quaerat, placeat adipisci deserunt ad quia explicabo quam culpa cupiditate quas numquam quod quidem, suscipit aut sint dignissimos ipsam. Enim!</p>
+            </div>
+        </main>
+    );
+}
