676c249f0e092c238ae213284d7bcbd63eea8547
matheuspsfranco <mathfort991@gmail.com>
2026-08-18 20:57:58 -0300

    feat : Adição dop resto do conteudo.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo() {
+    return (
+        <main>
+            <h2>Bem-vindo ao Meu Boiler Plate!</h2>
+            <p>Este é um exemplo de boilerplate para projetos React.</p>
+        </main>
+    )
+}
