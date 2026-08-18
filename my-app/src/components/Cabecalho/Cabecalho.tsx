8dc52e37ffca91a1c4a91b4129fc19b503bb3254
MatheusLuca <luca_matheus@live.com>
2026-08-18 20:54:55 -0300

    feat: criacao do componente cabecalho

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,17 @@
+export default function Cabecalho(){
+
+    return (
+        <header>
+            <h1>Meu Boiler Plate</h1>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
