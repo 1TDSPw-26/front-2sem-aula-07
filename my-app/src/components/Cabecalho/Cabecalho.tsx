cb1058e29d572e962770bb3c635cccd47045ab92
PHCN <pedrohenrique.cn07@gmail.com>
2026-08-18 20:57:49 -0300

    feat: adicionando código em App.tsx, alterando a main.tsx e criando componentes Rodape, Conteudo e Cabecalho

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+    return (
+        <header>
+        <h1>Meu Boiler Plate</h1>
+        <nav>
+          <ul>
+            <li><a href="a">Item-1</a></li>
+            <li><a href="b">Item-2</a></li>
+            <li><a href="c">Item-3</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis blanditiis eligendi molestias aut, minus nulla libero sint iure sed. Aliquam, impedit modi? Tempora unde sed pariatur reiciendis? Voluptatem, optio ipsa.</p>
+        </main>
+    )
+}
