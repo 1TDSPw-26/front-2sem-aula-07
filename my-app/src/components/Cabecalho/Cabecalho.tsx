e56711d49778e8df0066d19795a4c9c56f66c623
devlipee <sluizfelipe692@gmail.com>
2026-08-18 20:57:54 -0300

    feat: adiciona cabeçalho

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,17 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Meu boiler plate</h1>
+
+            <ul>
+                <li><a href=""> Item 1</a></li>
+                <li><a href=""> Item 2</a></li>
+                <li> <a href="">Item 3</a></li>
+            </ul>
+
+
+
+
+        </header>
+    )
+}
