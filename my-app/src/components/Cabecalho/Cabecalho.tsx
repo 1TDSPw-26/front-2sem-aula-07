40b6e692e5ea19d1ca039e8f44bd245a6ef53122
devfcoelho <dafonsecafelipecoelho@gmail.com>
2026-08-18 20:57:50 -0300

    feat:  adiciona cabecalho

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,14 @@
+export default function Cabecalho(){
+    return(
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Item-1</a></li>
+                    <li><a href="#">Item-2</a></li>
+                    <li><a href="#">Item-3</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
