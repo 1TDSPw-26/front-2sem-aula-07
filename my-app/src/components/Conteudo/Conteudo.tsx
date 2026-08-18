1f82107f614e9081589001ceac91429fc318a2eb
proc0x41 <20628001+proc0x41@users.noreply.github.com>
2026-08-18 20:58:09 -0300

    feat: Página inicial App com componentização

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,19 @@
+export default
+    function Conteudo() {
+    return (
+        <main>
+            <div>
+                <h2>Conteudo 1</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore eaque fuga fugiat esse eveniet. Et animi placeat doloremque quae consequuntur nihil neque, optio voluptatibus, obcaecati, possimus minima debitis nostrum tempora.</p>
+            </div>
+            <div>
+                <h2>Conteudo 2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, saepe atque. Sit iste rerum pariatur ipsum, facere vel nostrum ratione autem. Et eveniet sapiente, nisi ut reiciendis velit architecto doloribus.</p>
+            </div>
+            <div>
+                <h2>Conteudo 3</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, tenetur vel velit cumque debitis incidunt blanditiis, eligendi quia, ut libero ab rem. Quisquam accusamus animi, eum veniam necessitatibus facere ad.</p>
+            </div>
+        </main>
+    );
+}
