f2117067f3431a5bcec984b82e970e1b20dbeefa
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-08-18 20:55:59 -0300

    feat: Import de divisões padrão de um html (header, main e footer). - Aula 18-08-2026.

---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+
+    return (
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Item 01</a></li>
+                    <li><a href="#">Item 02</a></li>
+                    <li><a href="#">Item 03</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
