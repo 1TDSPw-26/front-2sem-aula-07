a2560497b5391665c4a80526c4511c10780605a8
Ludmylla1905 <ludmilanogueira231@gmail.com>
2026-08-18 21:05:05 -0300

    feat: integrando componentes com modularização

---
+++ my_app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo(){
+
+    return(
+        <main>
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores repellendus eum est voluptate, accusamus ratione inventore eligendi impedit iste ipsam nostrum quia, qui atque, a optio animi exercitationem similique laborum?</h2>
+        </main>
+    );
+}
