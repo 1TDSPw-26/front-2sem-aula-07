d79231436a6022625f795ac67742a60217336198
rodrigobanharelli <guigo.banharelli@gmail.com>
2026-08-18 21:13:15 -0300

    feat: introdução do boiler plate

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,15 @@
+
+export default function Cabecalho() {
+  return (
+    <header>
+            <h1>Meu boiler plate</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                </ul>
+            </nav>   
+    </header>
+  )
+}
