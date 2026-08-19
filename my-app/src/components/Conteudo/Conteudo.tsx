5ea03e081c3909ae85d1acd05a3e1cecc15df962
Pedronuf <pp23104554@gmail.com>
2026-08-18 21:03:33 -0300

    feat: Integrando componentes com modularização.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,12 @@
+export default function Conteudo(){
+
+    return(
+        <main>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores! Aut, placeat. Earum impedit, rem culpa voluptatum veniam libero illum doloremque numquam dolorem quia aperiam exercitationem obcaecati perferendis eius sapiente.</h2>
+        </main>
+
+
+    );
+
+
+}
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,6 @@
+export default function Rodape() {
+
+    return (
+        <h2>Todos os direitos reservados &copy;</h2>
+    );
+}
