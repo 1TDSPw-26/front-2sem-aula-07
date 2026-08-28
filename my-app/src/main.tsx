1f0e2d2a07c104fb53b215e88498c5be5106e0c5
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-08-27 22:42:46 -0300

    feat: Rotas basicas, modulares e imagem.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-
 //Importar os objetos de rotas:
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
@@ -11,14 +10,15 @@ import EditarProdutos from './routes/EditarProdutos/index.tsx';
 import ErrorPage from './routes/ErrorPage/index.tsx';
 import Home from './routes/Home/index.tsx';
 import Produtos from './routes/Produtos/index.tsx';
+import Conteudo from './routes/Conteudo/index.tsx';
 
 //Montar um objeto que retorna uma props com as rotas:
 const router = createBrowserRouter([
   {path: '/', element: <App />, errorElement: <ErrorPage />,children:[
     {path: '/', element: <Home />},
     {path: '/produtos', element: <Produtos />},
-    {path: '/editar-produtos', element: <EditarProdutos />},
-    {path: '*', element: <ErrorPage />}
+    {path: '/editar-produtos/:id', element: <EditarProdutos />},
+    {path:"/churros", element:<Conteudo/>}
   ]}
 ]);
 
