81b3be82c4049fe8b22485c6cb34768bfc9ca2c8
PHCN <pedrohenrique.cn07@gmail.com>
2026-08-27 23:16:28 -0300

    feat: adicionando o tailwind e estilizando páginas

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router";
 export default function Cabecalho() {
 
     return (
-        <header>
+        <header className="bg-amber-900">
             <h1>Meu Boiler Plate</h1>
             <nav>
-                <ul>
+                <ul className="flex space-x-4">
                     <li><Link  to="/editar-produtos/232">Editar Produtos</Link></li>
                     <li><Link  to="/produtos">Produtos</Link></li>
                     <li><Link  to="/">Home</Link></li>
--- my-app/src/components/Rodape/Rodape.tsx
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Rodape() {
     return (
-        <footer>
+        <footer className="bg-amber-900 text-center">
             <p>Todos os direitos reservados 2026 &Copy;</p>
         </footer>
     )
